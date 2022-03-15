2fdd28cc69e95e680d8183bca9301545eacedf88
vidhya065v <vidhya065vidarba@gmail.com>
2022-03-15 14:28:57 +0530

    Three Project

---
+++ src/org/onetwo/Three.java
@@ -0,0 +1,16 @@
+package org.onetwo;
+
+public class Three {
+	
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		//driver.get("https://www.facebook.com/");
+		driver.navigate().to("\"https://www.facebook.com/\"");
+		String currentUrl = driver.getCurrentUrl();
+		
+	}
+	}
+
+
+}
